fff9b08716e706fd2d14c23ed2b25d92aa66a32b
Tanner B. C. <87777314+tanrbobanr@users.noreply.github.com>
2024-08-13 19:16:00 -0700

    Update nth_dow.py

--- utilities/nth_dow.py
+++ utilities/nth_dow.py
@@ -1,9 +1,11 @@
 def nth_dow(
-    year: int, month: int, dow: int, n: int
+    year: int, month: int, dow: int, n: int, day: int = 1
 ) -> datetime.date:
     """Get the Nth day of week for a given year and month. `n` starts
-    at `1`.
+    at `1`. If `day` is given, it will be used for the starting date (
+    e.g. if `day` lands on a Sunday, and `n=1`, the next Sunday will be
+    selected).
 
     """
-    d = datetime.date(year, month, 1)
+    d = datetime.date(year, month, day)
     return d.replace(day=(dow - d.weekday()) % 7 + (n - 1) * 7 + 1)
