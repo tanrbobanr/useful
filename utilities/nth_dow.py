609da4fe35630646d11aff9ea44cb28663f3163d
Tanner B. C. <87777314+tanrbobanr@users.noreply.github.com>
2024-08-13 19:11:09 -0700

    Create nth_dow.py

---
+++ utilities/nth_dow.py
@@ -0,0 +1,9 @@
+def get_nth_dow(
+    year: int, month: int, dow: int, n: int
+) -> datetime.date:
+    """Get the Nth day of week for a given year and month. `n` starts
+    at `1`.
+
+    """
+    d = datetime.date(year, month, 1)
+    return d.replace(day=(dow - d.weekday()) % 7 + (n - 1) * 7 + 1)
